11a60ffe261db95010fbfcd53a11345049f6f747
hardik <hardik22oct@gmail.com>
2025-10-07 01:43:44 +0530

    Register is connected to frontend

--- server.js
+++ server.js
@@ -9,11 +9,13 @@ import messageRoute from "./routes/message.route.js"
 import reviewRoute from "./routes/review.route.js"
 import authRoute from "./routes/auth.routes.js"
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 
 dotenv.config()
 const app = express();
 
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
 app.use((err,req,res,next)=>{
